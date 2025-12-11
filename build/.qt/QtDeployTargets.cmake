8743955c8d8c847c80cd8c479cea06cf072fb755
akwair <319286119@qq.com>
2025-12-11 19:50:10 +0800

    fix: 清理.gitignore忽略的冗余文件，更新追踪规则

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_cinema_order_FILE D:/CinemaOrder/build/cinema_order.exe)
-set(__QT_DEPLOY_TARGET_cinema_order_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_cinema_order_RUNTIME_DLLS D:/msys2/mingw64/bin/Qt6Sql.dll;D:/msys2/mingw64/bin/Qt6Widgets.dll;D:/msys2/mingw64/bin/Qt6Gui.dll;D:/msys2/mingw64/bin/Qt6Core.dll)
--- build/CMakeFiles/cinema_order.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for cinema_order.
-# This may be replaced when dependencies are built.
